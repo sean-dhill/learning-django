696645891067323f85754571141f571c0902fd12
Sean Dhillon <sean.dhill2004@gmail.com>
2025-07-25 21:06:44 -0700

    Added urls for additional months with simple goals

--- monthly_challenges/challenges/urls.py
+++ monthly_challenges/challenges/urls.py
@@ -3,5 +3,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("january", views.index)
+    path("january", views.january),
+    path("febuary", views.febuary),
+    path("march", views.march),
+    path("april", views.april)
 ]
--- monthly_challenges/challenges/views.py
+++ monthly_challenges/challenges/views.py
@@ -4,5 +4,14 @@ from django.http import HttpResponse
 # Create your views here.
 
 
-def index(request):
-    return HttpResponse("This works!")
+def january(request):
+    return HttpResponse("Set 3 personal intentions for the year.")
+
+def febuary(request):
+    return HttpResponse("Reach out to someone you haven't spoken to in a while.")
+
+def march(request):
+    return HttpResponse("Go outside for a 20-minute walk at least 3 times a per week")
+
+def april(request):
+    return HttpResponse("Declutter one space in your home")
