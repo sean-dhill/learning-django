7ecfd8384be688e2d3a13cd8608fcaf7777d83ca
Sean Dhillon <sean.dhill2004@gmail.com>
2025-07-29 00:18:10 -0700

    Added first view and urls

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 
 from . import views
 
-urlpattern = [
+urlpatterns = [
     path("", views.starting_page, name="starting-page"),
     path("/posts", views.posts, name="posts-page"),
     path("/posts/<slug>", views.post_detail,
