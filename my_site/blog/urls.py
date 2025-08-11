d38727baa0b574ca013763e00982ac51429e283b
Sean Dhillon <sean.dhill2004@gmail.com>
2025-08-10 20:02:37 -0700

    Added images and all post page

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -4,7 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.starting_page, name="starting-page"),
-    path("/posts", views.posts, name="posts-page"),
+    path("posts", views.posts, name="posts-page"),
     path("/posts/<slug>", views.post_detail,
          name="post-detail-page")  # /posts/my-first-post
 ]
