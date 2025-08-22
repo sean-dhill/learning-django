8e0ad27bc2bb75d00fcf18ca8aaada8906d8275b
Sean Dhillon <sean.dhill2004@gmail.com>
2025-08-21 17:40:50 -0700

    Overrode tag str method

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -6,6 +6,9 @@ from django.core.validators import MinLengthValidator
 class Tag(models.Model):
     caption = models.CharField(max_length=20)
 
+    def __str__(self):
+        return f"{self.caption}"
+
 
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
