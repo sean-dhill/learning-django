726056798e9061672924f139de0d6c645642e26b
Sean Dhillon <sean.dhill2004@gmail.com>
2025-08-21 17:37:00 -0700

    Corrected typo

--- my_site/blog/models.py
+++ my_site/blog/models.py
@@ -10,7 +10,7 @@ class Tag(models.Model):
 class Author(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
-    email_address = models.EmailField(null=True)\
+    email_address = models.EmailField(null=True)
     
     def __str__(self):
         return f"{self.first_name} {self.last_name}"
