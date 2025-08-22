510b6068be3c76a9d39bd0c01f78c0c8c1c8c1cf
Sean Dhillon <sean.dhill2004@gmail.com>
2025-08-21 17:53:40 -0700

    Auto complete slug admin side

--- my_site/blog/admin.py
+++ my_site/blog/admin.py
@@ -7,6 +7,7 @@ from .models import Author, Post, Tag
 class PostAdmin(admin.ModelAdmin):
     list_filter = ("author", "tags", "date",)
     list_display = ("title", "date", "author",)
+    prepopulated_fields = {"slug": ("title",)}
 
 admin.site.register(Post, PostAdmin)
 admin.site.register(Author)
