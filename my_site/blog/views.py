77afca1c5ad3c10269399a83319223a9163e7742
Sean Dhillon <sean.dhill2004@gmail.com>
2025-08-24 21:01:51 -0700

    Converted page detail view to DetailView

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render,  get_object_or_404
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 
 from .models import Post
 
@@ -23,9 +23,12 @@ class AllPostsView(ListView):
     ordering = ["-date"]
     context_object_name = "all_posts"
 
-def post_detail(request, slug):
-    identified_post = Post.objects.get(slug = slug)
-    return render(request, "blog/post-detail.html", {
-        "post": identified_post,
-        "post_tags": identified_post.tags.all()
-    })
+class PostDetailView(DetailView): #Django auto finds by slug if we have it set in URL
+    template_name = "blog/post-detail.html"
+    model = Post
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["post_tags"] = self.object.tags.all()
+        return context
+
