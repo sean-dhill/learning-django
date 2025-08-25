2d4eea35077fa6e85352e086fa1502d834c6d29c
Sean Dhillon <sean.dhill2004@gmail.com>
2025-08-24 20:53:00 -0700

    Converted starting page view to ListView

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,21 +1,21 @@
-from datetime import date
-
 from django.shortcuts import render,  get_object_or_404
+from django.views.generic import ListView
 
 from .models import Post
 
 
-
-def get_date(post):
-    return post['date']
 # Create your views here.
 
+class StartingPageView(ListView):
+    template_name = "blog/index.html"
+    model = Post
+    ordering = ["-date"]
+    context_object_name = "posts"
 
-def starting_page(request):
-    latest_posts = Post.objects.all().order_by("-date")[:3]
-    return render(request, "blog/index.html",{
-        "posts": latest_posts
-    })
+    def get_queryset(self):
+        queryset = super().get_queryset()
+        data = queryset[:3]
+        return data
 
 def posts(request):
     all_posts = Post.objects.all()
