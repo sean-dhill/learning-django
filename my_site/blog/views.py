5ea2af0a3adfa2433159e70f1d8afd9fbace5fda
Sean Dhillon <sean.dhill2004@gmail.com>
2025-08-25 12:07:24 -0700

    Added POST functionality to comment section

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render,  get_object_or_404
-from django.views.generic import ListView, DetailView
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views.generic import ListView
+from django.views import View
 
 from .models import Post
 from .forms import CommentForm
@@ -24,13 +27,31 @@ class AllPostsView(ListView):
     ordering = ["-date"]
     context_object_name = "all_posts"
 
-class PostDetailView(DetailView): #Django auto finds by slug if we have it set in URL
-    template_name = "blog/post-detail.html"
-    model = Post
+class PostDetailView(View): #Django auto finds by slug if we have it set in URL
+ 
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        context["comment_form"] = CommentForm()
-        return context
+    def get(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm
+        }
+        return render(request, "blog/post-detail.html", context)
 
+
+    def post(self, request, slug):
+        post = Post.objects.get(slug=slug)
+        comment_form = CommentForm(request.POST) #request.POST contains submitted data
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page", args=[slug]))
+        
+        context = {
+            "post": post,
+            "post_tags": post.tags.all(),
+            "comment_form": CommentForm
+        }
+        return render(request, "blog/post-detail.html", context)
